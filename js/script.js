9337d712364921b89aac021bebefea044ef5ce55
MicheleRai <rainonemichele1999@gmail.com>
2022-11-11 14:20:34 +0100

    all script and bonus added

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
 const app = new Vue({
         el: '#root',
         data:{
-            
-        }
-};)
+            title:'SIETE PRONTI AD INIZIARE IL VIAGGIO?',
+            titleClass:'center-title',
+            img:'img/avventura.jpg',
+            imgClass:'center-img'
+        },
+});
